97cacf9a5de1d0ff0c85fbc1b42dca3b7b8d4e1b
Michal Sadowski <michaleksadowski@gmail.com>
2017-05-18 11:53:06 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -6,6 +6,6 @@ app.controller('divCtrl', function ($scope, $http) {
 });
 app.controller('div2017Ctrl', function ($scope, $http) {
     $http.get('dywidendy2017.json').success(function (data) {
-        $scope.dividends = data.dywidendy2017;
+        $scope.dividends2017 = data.dywidendy2017;
     });       
 });
