d3b86a42295fead9dcd8f14b49234bb136069aee
Michal Sadowski <michaleksadowski@gmail.com>
2017-05-18 15:59:23 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -13,7 +13,7 @@ app.config(function($routeProvider) {
     });
 });
 */
-   .when("/dywidenda2017/:company", {
+   .when("/dywidenda2017/:companyID", {
         templateUrl : "company.html",
         controller : "compCtrl"
     });
@@ -35,7 +35,7 @@ app.controller('compCtrl', function ($scope, $routeParams, $http) {
 
 app.controller('compCtrl', function ($scope, $routeParams, $http) {
     $http.get('dywidendy2017.json').success(function (data) {
-        $scope.dividend = data.dywidendy2017[$routeParams.company];
+        $scope.dividend = data.dywidendy2017[$routeParams.companyID];
     });
 
 });
