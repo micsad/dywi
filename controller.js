dee5f2aacaabd0641352023d79faad900ebf0e1e
Michal Sadowski <michaleksadowski@gmail.com>
2017-05-18 14:42:26 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -11,12 +11,6 @@ app.config(function($routeProvider) {
     });
 });
 
-app.controller('divCtrl', function ($scope, $http) {
-    $http.get('datatable1.json').success(function (data) {
-        $scope.dividends = data.dividends2017;
-    });       
-});
-
 app.controller('div2017Ctrl', function ($scope, $http) {
     $http.get('dywidendy2017.json').success(function (data) {
         $scope.dividends2017 = data.dywidendy2017;
